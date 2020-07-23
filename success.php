72c7790d88de6c122e38a32dea859a81a9b9c920
daniel@dp07daniel.com <daniel@dp07daniel.com>
2020-07-23 12:05:40 -0300

    Creación back_urls en blanco

---
+++ success.php
@@ -0,0 +1,16 @@
+    <div class="modal-dialog modal-confirm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="icon-box">
+                    <i class="material-icons"></i>
+                </div>              
+                <h4 class="modal-title w-100">Éxito!</h4> 
+            </div>
+            <div class="modal-body">
+                <p class="text-center">Tu compra fue realizada exitosamente.</p>
+            </div>
+            <div class="modal-footer">
+                <a class="btn btn-success btn-block" href="<?php echo '../index.php'; ?>">Regresar a la tienda</a>
+            </div>
+        </div>
+    </div>

817a5ba3975ac05347d8af114fec863c20ad95e3
daniel@dp07daniel.com <daniel@dp07daniel.com>
2020-07-23 16:16:33 -0300

    Correcciones menores

--- endpoint.php
+++ endpoint.php
@@ -5,7 +5,7 @@
 
 $webhookContent = "";
 
-$webhook = fopen('php://input' , 'rb');
+$webhook = fopen('php://input' , 'r');
 while (!feof($webhook)) {
        $webhookContent .= fread($webhook, 4096);
 }
@@ -16,55 +16,8 @@ file_put_contents(
   json_encode($webhookContent) . PHP_EOL,
   FILE_APPEND
 );
-$msg = "prueba-".json_encode($webhookContent);
+$msg = json_encode($webhookContent);
 mail("dapazos.89@gmail.com","Información recibida", $msg);
-/*
-// SDK de Mercado Pago
-require __DIR__ .  '/vendor/autoload.php';
 
-    MercadoPago\SDK::setAccessToken("TEST-8049437300582624-072304-d7af7ea61959047b81dd8b379f76ebdc-148176642");
-
-    switch($_POST["type"]) {
-        case "payment":
-            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-            break;
-        case "plan":
-            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-            break;
-        case "subscription":
-            $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
-            break;
-        case "invoice":
-            $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
-            break;
-    }
-
-    echo "<pre>";
-    print_r($payment);
-    print_r($plan);
-    echo "</pre>";
-
-    if (isset($payment)) {
-        $dp_pay = json_encode($payment);
-        $arpay = fopen("downloads/".$_POST["id"].".txt", "a") or
-          die();
-        fputs($arpay, $dp_pay);
-        fputs($arpay, "\n");
-        fputs($arpay, "--------------------------------------------------------");
-        fputs($arpay, "\n");
-        fclose($arpay);
-    }
-
-    if (isset($plan)) {
-        $dp_plan = json_encode($plan);
-        $arplan = fopen("downloads/".$_POST["id"].".txt", "a") or
-          die();
-        fputs($arplan, $dp_plan);
-        fputs($arplan, "\n");
-        fputs($arplan, "--------------------------------------------------------");
-        fputs($arplan, "\n");
-        fclose($arplan);
-    }
-*/
 
 ?>
